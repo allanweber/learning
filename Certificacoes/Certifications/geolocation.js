2006f6b50fdfe7ca9222352dcdd6ac1c8fbe9ada
ALLAN Cassiano Weber <acweber@sispro.com.br>
2016-07-26 10:58:04 -0300

    geo com google maps

--- Certificacoes/Certifications/geolocation.js
+++ Certificacoes/Certifications/geolocation.js
@@ -1,21 +1,84 @@
-var watcher;
 var geoLocator;
+
+class locationClass {
+    constructor() {
+        var self = this;
+        self.latitude = 0;
+        self.longitude = 0;
+    }
+}
+
+function initMap() {
+
+    if (localStorage.length == 0) return;
+
+    var loc = JSON.parse(localStorage.getItem(localStorage.key(localStorage.length - 1)));
+
+    var myLatLng = { lat: loc.latitude, lng: loc.longitude };
+
+    var map = new google.maps.Map(document.getElementById('mapholder'), {
+        zoom: 17,
+        center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Você está aqui!'
+    });
+
+}
+
 window.onload = function () {
-        geoLocator = window.navigator.geolocation;
-        var posOptions = { enableHighAccuracy: true, timeout: 3000 };
+    localStorage.clear();
+    window.setInterval(loadLocation, 60000);
 
-        watcher = geoLocator.watchPosition(successPosition, errorPosition, posOptions);
-}
-function successPosition(pos) {
-        var sp = document.createElement("p");
-        sp.innerText = "Latitude: " + pos.coords.latitude + " Longitude: "
-            + pos.coords.longitude;
-        document.getElementById("geoResults").appendChild(sp);
-        geoLocator.clearWatch(watcher);
-}
+    function loadLocation() {
+        geoLocator = window.navigator.geolocation;
+        geoLocator.getCurrentPosition(successPosition);
+    }
 
-function errorPosition(err) {
-        var sp = document.createElement("p");
-        sp.innerText = "error: " + err.message; + " code: " + err.code;
-        document.getElementById("geoResults").appendChild(sp);
+    function successPosition(pos) {
+
+        if (localStorage.length >= 10)
+            localStorage.clear();
+
+        var loc = new locationClass()
+        loc.latitude = pos.coords.latitude;
+        loc.longitude = pos.coords.longitude;
+
+        localStorage.setItem(localStorage.length + 1, JSON.stringify(loc));
+        loadStorage();
+        initMap();
+    }
+
+    function errorPosition(err) {
+        var sp = document.createElement("p");
+        sp.innerText = "error: " + err.message; + " code: " + err.code;
+        document.getElementById("geoResults").appendChild(sp);
+    }
+
+    function loadStorage() {
+        var table = document.createElement("table");
+        table.id = "tbLocation";
+        document.getElementById("tbLocation").remove();
+        document.getElementById("locationDiv").appendChild(table);
+
+        if (localStorage.length > 0) {
+            for (var index = 0; index < localStorage.length; index++) {
+                var loc = JSON.parse(localStorage.getItem(localStorage.key(index)));
+
+                var row = document.createElement("tr");
+                var long = document.createElement("td");
+                var lat = document.createElement("td");
+
+                long.innerText = loc.longitude;
+                lat.innerText = loc.latitude;
+
+                row.appendChild(long);
+                row.appendChild(lat);
+                table.appendChild(row)
+            }
+        }
+    }
 }
